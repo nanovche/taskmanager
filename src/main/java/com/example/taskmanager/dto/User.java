51a1689903a045becda8c70d484f473ad472fa20
nanovche <nanovche1@gmail.com>
2025-11-04 00:33:29 +0200

    fetchuserbyid in repo

--- src/main/java/com/example/taskmanager/dto/User.java
+++ src/main/java/com/example/taskmanager/dto/User.java
@@ -1,14 +1,14 @@
 package com.example.taskmanager.dto;
 
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 
 @Getter
 @Setter
-@NoArgsConstructor
 @AllArgsConstructor
-@ToString
-@EqualsAndHashCode
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class User {
     private Long id;
     private String username;
--- src/main/java/com/example/taskmanager/repository/UserRepository.java
+++ src/main/java/com/example/taskmanager/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package com.example.taskmanager.repository;
 
+import com.example.taskmanager.dto.User;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
 
 @Repository
 public class UserRepository {
@@ -11,10 +13,24 @@ public class UserRepository {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public String findUsernameById(Long id) {
+    public String fetchUsernameById(Long id) {
         return jdbcTemplate.queryForObject(
                 "SELECT username FROM users WHERE id = ?",
                 String.class,
                 id);
+    }
+
+    public User fetchUserById(Long id) {
+        return jdbcTemplate.queryForObject(
+                "SELECT id,username,password FROM users WHERE id = ?",
+                (rs, rowNum) -> {
+                    User user = new User();
+                    user.setId(Long.valueOf(rs.getString("id")));
+                    user.setUsername(rs.getString("username"));
+                    user.setPassword(rs.getString("password"));
+                    return user;
+                },
+                id
+        );
     }
 }
