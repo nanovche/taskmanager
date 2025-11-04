f537746ae15065b15188ad330e673b588ac84853
nanovche <nanovche1@gmail.com>
2025-11-04 22:51:45 +0200

    refactored fetchuser with usermapper

--- src/main/java/com/example/taskmanager/service/UserService.java
+++ src/main/java/com/example/taskmanager/service/UserService.java
@@ -1,54 +1,47 @@
 package com.example.taskmanager.service;
 
-import com.example.taskmanager.dto.User;
+import com.example.taskmanager.dto.UserDTO;
+import com.example.taskmanager.entity.UserEntity;
 import com.example.taskmanager.exception.NoSuchUserException;
-import com.example.taskmanager.exception.UserCreationException;
-import com.example.taskmanager.exception.UserAlreadyExistsException;
 import com.example.taskmanager.exception.WeakPasswordException;
+import com.example.taskmanager.mappers.UserMapper;
 import com.example.taskmanager.repository.UserRepository;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 @Service
 public class UserService {
 
-    private final JdbcUserDetailsManager jdbcUserDetailsManager;
+    private final UserMapper userMapper;
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserService(JdbcUserDetailsManager jdbcUserDetailsManager, UserRepository userRepository, PasswordEncoder passwordEncoder) {
-        this.jdbcUserDetailsManager = jdbcUserDetailsManager;
+    public UserService(UserMapper userMapper, UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userMapper = userMapper;
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
-    public User fetchUser(Long userId) {
+    public UserDTO fetchUser(Long userId) {
 
         if (userId == null) {
             throw new IllegalArgumentException("userid is null");
         }
-        User user;
-        try {
-            user = userRepository.fetchUserById(userId);
-        } catch (DataAccessException ex) {
-            throw new NoSuchUserException(String.format("user with id %s does not exist", userId), ex);
-        }
-        return user;
+
+        return userRepository.findById(userId)
+                .map(userMapper::toDto)
+                .orElseThrow(() -> new NoSuchUserException(String.format("user with id %s does not exist", userId)));
     }
 
-    public User createUser(User dtoUser) {
+    /* when replace jdbc manager with jpa repo -> return obj with id from repo and add id to .created(uri) */
+    public UserDTO createUser(UserDTO dtoUser) {
 
-        if (dtoUser == null || StringUtils.isEmpty(dtoUser.getUsername()) || StringUtils.isEmpty(dtoUser.getPassword())) {
+/*        if (dtoUser == null || StringUtils.isEmpty(dtoUser.getUsername()) || StringUtils.isEmpty(dtoUser.getPassword())) {
             throw new IllegalArgumentException("insufficient user data");
         }
 
@@ -57,17 +50,18 @@ public class UserService {
         }
 
         if(jdbcUserDetailsManager.userExists(dtoUser.getUsername())) {
-            throw new UserAlreadyExistsException(String.format("User %s already exists", dtoUser.getUsername()));
+            throw new UserAlreadyExistsException(String.format("UserDTO %s already exists", dtoUser.getUsername()));
         } else {
             if(checkPasswordStrength(dtoUser.getPassword())) {
-                UserDetails newUser = org.springframework.security.core.userdetails.User
+                UserDetails newUser = org.springframework.security.core.userdetails.UserDTO
                         .withUsername(dtoUser.getUsername())
                         .password(passwordEncoder.encode(dtoUser.getPassword()))
                         .authorities(Arrays.stream(dtoUser.getAuthorities()).map(SimpleGrantedAuthority::new).collect(Collectors.toList()))
                         .build();
 
                 try {
-                    jdbcUserDetailsManager.createUser(newUser);
+                    UserDTO user = userRepository.save(newUser);
+//                    jdbcUserDetailsManager.createUser(newUser);
                 } catch (DataAccessException ex) {
                     throw new UserCreationException("failed to create user", ex);
                 }
@@ -75,7 +69,8 @@ public class UserService {
             } else {
                 throw new WeakPasswordException("weak password");
             }
-        }
+        }*/
+        return null;
     }
 
     public void changePassword(String newPassword, String username){
@@ -92,33 +87,9 @@ public class UserService {
         }
     }
 
-    public void updateUser(User userDto) {
-
-        //double check validation?
-        if (userDto == null || StringUtils.isEmpty(userDto.getUsername()) || StringUtils.isEmpty(userDto.getPassword())) {
-            throw new IllegalArgumentException("insufficient user data");
-        }
-
-        if (userDto.getAuthorities() == null || userDto.getAuthorities().length == 0) {
-            throw new IllegalArgumentException("no authorities data");
-        }
-
-        UserDetails existingDbUser = jdbcUserDetailsManager.loadUserByUsername(userDto.getUsername());
-
-//        needsUpdate()?
-        //check if needs update -> check if different from one in db?
-        UserDetails updatedUser = org.springframework.security.core.userdetails.User
-                .withUsername(userDto.getUsername())
-                .passwordEncoder(passwordEncoder::encode)
-                .authorities(Arrays.stream(userDto.getAuthorities()).map(SimpleGrantedAuthority::new).collect(Collectors.toList()))
-                .build();
-
-        jdbcUserDetailsManager.updateUser(updatedUser);
-    }
-
     public void deleteUser(Long userId) {
 
-        if (userId == null) {
+/*        if (userId == null) {
             throw new IllegalArgumentException("userId is null");
         }
 
@@ -128,7 +99,7 @@ public class UserService {
         } catch (DataAccessException ex) {
             throw new NoSuchUserException(String.format("user with id %s does not exist - no user is deleted", userId), ex);
         }
-        jdbcUserDetailsManager.deleteUser(username);
+        jdbcUserDetailsManager.deleteUser(username);*/
     }
 
     private boolean checkPasswordStrength(String password){
