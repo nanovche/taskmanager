41fe14552f9dcc0d61d71da0bb6b064ca83dd611
nanovche <nanovche1@gmail.com>
2025-11-05 11:53:59 +0200

    remove unnecesarry files

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `spring`.`authorities` VALUES (NULL, 'john', 'ROLE_ADMIN')
-ON DUPLICATE KEY UPDATE username = VALUES(username);
-
- INSERT INTO `spring`.`users` VALUES (NULL, 'john', '$2a$10$l/M6B4pl8Iq7mqX3KrrV0OGfJjTgjq7rdBxRie5TrsApQUOXEgr/u', '1')
- ON DUPLICATE KEY UPDATE username = VALUES(username);
